1cde648a0c7e8b472bbc6f58ab45de6e2ae1046d
realtvop <realtvop@realtvop.top>
2026-06-25 02:33:31 +0800

    feat(remote-control): render HomeDevicePlaybackOverview on home page

--- src/app/pages/home.tsx
+++ src/app/pages/home.tsx
@@ -6,7 +6,7 @@ import {
 } from "@/app/components/fallbacks/home-fallbacks";
 import { MobilePageHeader } from "@/app/components/header/mobile-page-header";
 import HomeHeader from "@/app/components/home/carousel/header";
-import { CrossDevicePlaybacks } from "@/app/components/home/cross-device-playbacks";
+import { HomeDevicePlaybackOverview } from "@/app/components/remote-control/home-overview";
 import PinnedList from "@/app/components/home/pinned-list";
 import PreviewList from "@/app/components/home/preview-list";
 import {
@@ -97,7 +97,7 @@ export default function Home() {
           showUserDropdown
         />
 
-        <CrossDevicePlaybacks />
+        <HomeDevicePlaybackOverview />
 
         {isFetching || isLoading ? (
           <HeaderFallback />
